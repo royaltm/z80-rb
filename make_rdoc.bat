8ffdfc7c670d44f420d03c1226bbd3a70b68def8
Rafał Michalski <royaltm75@gmail.com>
2019-02-10 00:15:35 +0100

    make rdoc gh-pages

--- make_rdoc.bat
+++ make_rdoc.bat
@@ -1,1 +1,7 @@
 rdoc --encoding=UTF-8 --title=ruby-Z80 --main=README.rdoc README.rdoc z80.rb z80/*.rb zxlib/*.rb utils/*.rb
+rem cd doc
+rem mkdir -p examples
+rem cp -v ../examples/*.{jpg,png} examples/
+rem git init
+rem git remote add -t 'gh-pages' -f origin git@github.com:royaltm/z80-rb.git
+rem git checkout 'gh-pages'
