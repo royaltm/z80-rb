fb62379a24eba844c412a03ed326eaf43ace1c03
Rafal Michalski <royaltm75@gmail.com>
2020-10-15 13:13:23 +0200

    make_rdoc.bat updated to match rake doc task

--- make_rdoc.bat
+++ make_rdoc.bat
@@ -1,4 +1,4 @@
-rdoc --encoding=UTF-8 --title=ruby-Z80 --main=README.rdoc README.rdoc z80.rb z80/*.rb zxlib/*.rb utils/*.rb
+rdoc --encoding=UTF-8 --title=ruby-Z80 --main=README.rdoc README.rdoc LICENSE.md lib/z80.rb lib/z80/*.rb lib/z80/utils/*.rb lib/zxlib/*.rb lib/zxlib/*/*.rb lib/zxutils/*.rb lib/zxutils/*/*.rb
 rem cd doc
 rem mkdir -p examples
 rem cp -v ../examples/*.{jpg,png} examples/
